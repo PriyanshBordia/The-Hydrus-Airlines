ab4d47d511ba17f5fee6162715923878fa6a8da8
PriyanshBordia <priyanshbordia1@gmail.com>
2020-11-16 21:17:30 +0530

    _ removed label gramatic errors removed

--- Cancel.java
+++ Cancel.java
@@ -9,7 +9,7 @@ public class Cancel extends Add_Customer
 {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
@@ -23,19 +23,19 @@ public class Cancel extends Add_Customer
 	}
 
 	private void init() {
-		
-		setTitle("TICKET Cancel");
-		
+
+		setTitle("Ticket Cancel");
+
 		setVisible(true);
 		setSize(1000,600);
 		setLocation(450,200);
-		
+
 		/*ImageIcon img = loadImage();
 		JLabel lab = new JLabel(img);
 		add(lab);
 		setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);*/
-		
+
 		Font f1,f2,f3,f4;
 		Label l1,l2,le,le2;
 		TextField tf1,tf2;
@@ -43,15 +43,15 @@ public class Cancel extends Add_Customer
 		GridBagLayout gl = new GridBagLayout();
 		GridBagConstraints gc = new GridBagConstraints();
 		setLayout(gl);
-		
-		
-		
+
+
+
 		f1 = new Font("Tahoma",Font.BOLD,50);
 		f2 = new Font("Tahoma",Font.PLAIN,34);
 		f3 = new Font("TimesRoman",Font.BOLD,120);
 		f4 = new Font("Tahoma",Font.PLAIN,28);
-		
-		
+
+
 		JLabel l0 = new JLabel("Cancel");
 		add(l0);
 		l0.setFont(f1);
@@ -61,55 +61,55 @@ public class Cancel extends Add_Customer
 		gc.gridy = 0;
 		gl.setConstraints(l0, gc);
 		//l0.setBounds(420, 60, 1000, 55);
-		
+
 		le = new Label(" ");
 		add(le);
 		le.setFont(f3);
 		gc.gridx = 0;
 		gc.gridy = 2;
 		gl.setConstraints(le, gc);
-		
-		l1 = new Label("PNR no.");
+
+		l1 = new Label("PNR No.");
 		l1.setFont(f2);
 		add(l1);
 		gc.gridx = 0;
 		gc.gridy = 4;
 		gl.setConstraints(l1, gc);
-		
-		
+
+
 		l2 = new Label("Password");
 		add(l2);
 		l2.setFont(f2);
 		gc.gridx = 0;
 		gc.gridy = 6;
 		gl.setConstraints(l2, gc);
-		
-		
+
+
 		tf1 = new TextField(20);
 		add(tf1);
 		gc.gridx = 1;
 		gc.gridy = 4;
 		gl.setConstraints(tf1, gc);
-		
+
 		tf2 = new TextField(20);
 		add(tf2);
 		gc.gridx = 1;
 		gc.gridy = 6;
 		gl.setConstraints(tf2, gc);
 		tf2.setEchoChar('*');
-		
+
 		/*tf3 = new TextField(20);
 		add(tf3);*/
-		
+
 		le2 = new Label(" ");
 		add(le2);
 		le2.setFont(f4);
 		gc.gridx = 0;
 		gc.gridy = 2;
 		gl.setConstraints(le2, gc);
-		
-		
-		
+
+
+
 		b1 = new Button("Submit");
 		add(b1);
 		b1.setFont(f4);
@@ -117,10 +117,10 @@ public class Cancel extends Add_Customer
 		gc.gridx = 1;
 		gc.gridy = 8;
 		gl.setConstraints(b1, gc);
-		
-		b1.addActionListener(new ActionListener() 
+
+		b1.addActionListener(new ActionListener()
 		{
-			public void actionPerformed(ActionEvent a) 
+			public void actionPerformed(ActionEvent a)
 			{
 				if(a.getSource()==b1)
 				{
--- MainScreen.java
+++ MainScreen.java
@@ -10,7 +10,7 @@ public class MainScreen extends JFrame implements ActionListener
 {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID2 = 1L;
 	private static final Object ReservationDetails = null;
@@ -19,115 +19,113 @@ public class MainScreen extends JFrame implements ActionListener
 	private static final Object SectorDetails = null;
 	private static final Object Cancellation = null;
 	private static final Object E_Ticket = null;
-	
+
 	public static void main(String[] args)
 	{
 		new MainScreen();
 	}
-	
+
 	public MainScreen()
 	{
 		super("AIRLINE RESERVATION SYSTEM");
-		
+
 		//Set the text color
 		setForeground(Color.white);
-		
+
 		setLayout(null);    // For custom layout
-		
+
 		//Layout for image.
 		JLabel L = new JLabel(" ");
 		Image img = new ImageIcon(this.getClass().getResource("/airplane-fact.jpg")).getImage();
-		L.setIcon(new ImageIcon(img)); 
-		L.setBounds(0 , 0 , 1980 , 990); 
+		L.setIcon(new ImageIcon(img));
+		L.setBounds(0 , 0 , 1980 , 990);
 		add(L);
-		
-		JLabel L2 = new JLabel("AIR INDIA WELCOMES YOU");
+
+		JLabel L2 = new JLabel("HYDRUS AIRLINES WELCOMES YOU");
 		L2.setForeground(Color.BLUE);
 	    L2.setFont(new Font("Aerial", Font.PLAIN , 36));
-		L2.setBounds(420, 60, 1000, 55); 
+		L2.setBounds(420, 60, 1000, 55);
 		L.add(L2);
-	
+
 		JMenuBar M = new JMenuBar();
 		setJMenuBar(M);
-		JMenu M1 = new JMenu("Details"); 
+		JMenu M1 = new JMenu("Details");
 		M1.setForeground(Color.BLACK);
 		M.add(M1);
-		
-        JMenuItem FlightDetails = new JMenuItem("FLIGHT_INFO");
+
+        JMenuItem FlightDetails = new JMenuItem("FLIGHT INFO");
         M1.add(FlightDetails);
-		
-        JMenuItem ReservationDetails = new JMenuItem("ADD_CUSTOMER_DETAILS");
+
+        JMenuItem ReservationDetails = new JMenuItem("ADD CUSTOMER DETAILS");
         M1.add(ReservationDetails);
-		
-        JMenuItem PassengerDetails = new JMenuItem("JOURNEY_DETAILS");
+
+        JMenuItem PassengerDetails = new JMenuItem("JOURNEY DETAILS");
         M1.add(PassengerDetails);
-		
-        JMenuItem SectorDetails = new JMenuItem("PAYMENT_DETAILS");
+
+        JMenuItem SectorDetails = new JMenuItem("PAYMENT DETAILS");
         M1.add(SectorDetails);
-		
+
         JMenuItem Cancellation = new JMenuItem("CANCELLATION");
         M1.add(Cancellation);
-		
+
         JMenu Ticket = new JMenu("TICKET");
         Ticket.setForeground(Color.BLACK);
         M.add(Ticket);
-		
-        JMenuItem E_Ticket = new JMenuItem("E_Ticket");
+
+        JMenuItem E_Ticket = new JMenuItem("E-Ticket");
         Ticket.add(E_Ticket);
-        
+
         FlightDetails.addActionListener(this);
         ReservationDetails.addActionListener(this);
         PassengerDetails.addActionListener(this);
         SectorDetails.addActionListener(this);
         Cancellation.addActionListener(this);
         E_Ticket.addActionListener(this);
-        
+
         setSize(1950,1090);
     	setVisible(true);
-        
+
 	}
-	
+
     	public void actionPerformed(ActionEvent a)
         {
         	if(a.getSource()==FlightDetails)
         	{
         		new Flight_Info();
         	}
-        	
+
         	if(a.getSource()==ReservationDetails)
         	{
-        		try 
+        		try
         		{
                     new Add_Customer();
-        		} 
-        		catch (Exception e) 
+        		}
+        		catch (Exception e)
         		{
                     e.printStackTrace();
-        		} 
+        		}
         	}
-        	
+
         	if(a.getSource()==PassengerDetails)
         	{
-        		try 
+        		try
         		{
                     new Journey_Details();
-                } 
-        		catch (Exception e) 
+                }
+        		catch (Exception e)
         		{
                     e.printStackTrace();
         		}
         	}
         	if(a.getSource()==Cancellation)
         	{
-        		new Cancel(); 
+        		new Cancel();
         	}
-        	
+
         	if(a.getSource()==E_Ticket)
         	{
-        		new E_Ticket(); 
+        		new E_Ticket();
         	}
-        	
+
         }
 }
-		
-	
