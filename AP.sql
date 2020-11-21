8e1a57d87bacbdf595b3727a76738be4b506d62f
PriyanshBordia <priyanshbordia1@gmail.com>
2020-11-21 22:49:46 +0530

    Delete AP.sql

--- AP.sql
+++
@@ -1,8 +0,0 @@
-use airline_database;
-
-SELECT p_code FROM passenger where passport_no = 123456 and name = "gaurav";
-
-select * from flight;
-
-INSERT INTO booking (f_code , p_code) VALUES ('JA9011', 1);
-
